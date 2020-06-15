edfab2da4f467c6b5cc818cf8129db305f33e91a
clark-lindsay <clarklindsay96@gmail.com>
2020-06-15 12:32:53 -0400

    Initial Commit

---
+++ Unit Converter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Unit Converter
+//
+//  Created by Clark Lindsay on 6/15/20.
+//  Copyright © 2020 Clark Lindsay. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
